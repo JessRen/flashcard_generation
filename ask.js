96ed9d4c5cea6e996d9d39a9f8d55e778d8c0e98
Jessica <jessicalrenard@gmail.com>
2017-10-09 17:46:05 -0400

    Easier to run, via StudyCards.js

---
+++ ask.js
@@ -0,0 +1,38 @@
+exports.questions = [
+	{
+		full: 'The color green is a combination of blue and yellow.',
+		cloze: 'yellow'
+	},
+	{
+		full: 'The color purple is a combination of blue and red.',
+		cloze: 'red'
+	},
+	{
+		full: 'Teal is a tertiary color',
+		cloze: 'tertiary'
+	},
+	{
+		full: 'Oiled bronze is an effect often enough found on Pottery Barn home decors that feature metal, and it can make an item look more antique.',
+		cloze: 'metal'
+	},
+	{
+		full: 'Reclaimed wood is wood that has been utilized in past built structures, like barns, ships, bowling alleys and other decking.',
+		cloze: 'Reclaimed'
+	},
+	{
+		full: 'An outhouse is a outdoor facility relied upon before public plumbing was established, to separate pottying from other faculties.',
+		cloze: 'outhouse'
+	},
+	{
+		full: 'Amun-Ra was worshipped as the sun god and chief diety across the Egyptian Empire.',
+		cloze: 'Amun-Ra'
+	},
+	{
+		full: 'A sedan comes with 2-doors or 4-doors.',
+		cloze: '4-doors'
+	},
+	{
+		full: 'Cinnamon is known to maintain blood sugar levels, and it likely helps to alleviates high blood pressure and cholesterol troubles too.',
+		cloze: 'Cinnamon'
+	}
+]
